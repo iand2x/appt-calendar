e6cdec3c5366193a3d64474c8bc932d6c5e67614
Andrew <andrewttm@live.com>
2025-08-03 00:22:22 +0800

    test font

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  // @ts-expect-error incompatibilities between ESLint built-in types and the types used by typescript-eslint
   tseslint.configs.recommended,
   pluginVue.configs["flat/essential"],
   { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
